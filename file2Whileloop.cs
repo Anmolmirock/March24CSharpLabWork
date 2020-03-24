b25e66d2e1d41c179753cc7546fd8016bfd3f4d8
Anmolmirock <43281173+Anmolmirock@users.noreply.github.com>
2020-03-24 10:55:03 -0400

    Create file2Whileloop.cs

---
+++ file2Whileloop.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Hello
+{
+    public static void Main()
+    {
+        int i = 1;
+        while (i < 10)
+        {
+
+            Console.WriteLine(i);
+            i++;
+
+        }
+
+        System.Console.WriteLine("Hello C#");
+    }
+}
