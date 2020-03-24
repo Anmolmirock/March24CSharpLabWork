b6e476ac582ef6f53bb81e32f5ffcab8109daae8
Anmolmirock <43281173+Anmolmirock@users.noreply.github.com>
2020-03-24 11:00:02 -0400

    Create file3Calculator.cs

---
+++ file3Calculator.cs
@@ -0,0 +1,43 @@
+using System;
+namespace program
+{
+    public class Hello
+    {
+        public static void Main()
+        {
+            calculator obj = new calculator();
+            Console.WriteLine("sum :" + obj.sum(50, 60));
+            Console.WriteLine("sub :" + obj.sub(100, 20));
+            Console.WriteLine("multiply :" + obj.multiply(80, 30));
+            Console.WriteLine("divide :" + obj.divide(10, 20));
+            Console.WriteLine("exponeniate :" + obj.exponeniate(10));
+
+        }
+        class calculator
+        {
+            
+
+            public double sum(double a, double b)
+            {
+                return a + b;
+            }
+            public double sub(double a, double b)
+            {
+                return a - b;
+            }
+            public double multiply(double a, double b)
+            {
+                return a * b;
+            }
+            public double divide(double a, double b)
+            {
+                return a / b;
+            }
+            public double exponeniate(double a)
+            {
+                return Math.Exp(a);
+            }
+        }
+
+    }
+}
