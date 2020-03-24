9bb4832fc6c56ef3f357db28d0fc054c220aea0a
Anmolmirock <43281173+Anmolmirock@users.noreply.github.com>
2020-03-24 10:31:45 -0400

    Create file1.cs

---
+++ file1.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Counting
+{
+    using System;
+
+
+    public class Hello
+    {
+
+        public static void Main()
+        {
+
+            int i = 1;
+
+            for (i=0;i<100 ;i++)
+
+            {
+
+                Console.WriteLine(i);
+
+            }
+
+
+
+            System.Console.WriteLine("Hello C#");
+
+        }
+
+    }
+}
